c8bf66a2fa01bae051b93d310f9f72cf96d97680
ROY <85047573+ROY-ID@users.noreply.github.com>
2022-04-18 14:03:15 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("meta").moch_yayan()
+       __import__("meta")cek_pw();moch_yayan()
    except Exception as e:
        exit(str(e))
